4c5fe653fdcc4655a576ac21f2265c8681d0cb7a
Adam Svalhede <adsv1212@storsjogymnasiet.se>
2023-11-07 11:28:29 +0100

    ea

---
+++ hej.py
@@ -0,0 +1,43 @@
+"""
+nummer = 2
+nummer2 = 5
+nummer3 = 8
+
+nummer = input()
+nummer2 = input()
+
+nummers = int(nummer) * int(nummer2)
+
+nummer3 = input()
+
+print(int(nummers) / int(nummer3))
+"""
+
+"""
+nummer = int(input())
+
+if nummer < 10:
+    print("Numret är mindre än 10")
+
+if nummer > 10:
+    print("Numret är större än 10")
+
+if nummer == 10:
+    print("Numret är 10")
+
+if nummer < 10:
+    print("Numret är mindre än 10")
+elif nummer > 10:
+    print("Numret är större än 10")
+else:
+    print("Numret är 10")
+    """
+
+nummer = int(input())
+nummer2 = int(input())
+
+if nummer < 100:
+    print(nummer * nummer2)
+
+if nummer > 100:
+    print(nummer / nummer2)
